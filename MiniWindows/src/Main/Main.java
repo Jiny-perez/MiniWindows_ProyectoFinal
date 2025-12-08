e3e75e7a7032b77307d013c589143493e600dc22
Jiny-perez <jinyperez55@gmail.com>
2025-12-08 02:50:10 -0600

    USUARIO NUEVO

--- MiniWindows/src/Main/Main.java
+++ MiniWindows/src/Main/Main.java
@@ -9,16 +9,17 @@ import javax.swing.UIManager;
  * @author marye
  */
 public class Main {
-   public static void main(String[] args) {
-       
-         try {       
+
+    public static void main(String[] args) {
+
+        try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             e.printStackTrace();
         }
-         
+
         SwingUtilities.invokeLater(() -> {
-                   SwingUtilities.invokeLater(() -> new VentanaInicio().MostrarVenatanaInicio());
+            SwingUtilities.invokeLater(() -> new VentanaInicio().MostrarVenatanaInicio());
 
         });
     }
